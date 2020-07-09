61a01e429b7aa2056714948eb4f2125b492ce0a8
novais <1180499@isep.ipp.pt>
2020-07-09 22:28:46 +0100

    added gba template files

---
+++ template/cmake-build-debug/CMakeFiles/template.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/PROJETO/template/source/template.c" "C:/PROJETO/template/cmake-build-debug/CMakeFiles/template.dir/source/template.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "MSVC")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "C:/devkitPro/libgba/include"
+  "C:/devkitPro/libtonc/include"
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ template/cmake-build-debug/CMakeFiles/template.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/template.dir/source/template.c.obj"
+  "template.exe"
+  "template.exe.manifest"
+  "template.lib"
+  "template.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/template.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
