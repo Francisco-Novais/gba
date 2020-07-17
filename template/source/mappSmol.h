1c795faf70fa661ede3497d7d765fdb36ba4b830
novais <francisconovais360@hotmail.com>
2020-07-17 05:57:04 +0100

    fixed collision; broke jumping

---
+++ template/source/mappSmol.h
@@ -0,0 +1,32 @@
+
+//{{BLOCK(mappSmol)
+
+//======================================================================
+//
+//	mappSmol, 256x256@4, 
+//	+ palette 256 entries, not compressed
+//	+ 35 tiles (t|f|p reduced) not compressed
+//	+ regular map (in SBBs), not compressed, 32x32 
+//	Total size: 512 + 1120 + 2048 = 3680
+//
+//	Time-stamp: 2020-07-15, 03:57:01
+//	Exported by Cearn's GBA Image Transmogrifier, v0.8.3
+//	( http://www.coranac.com/projects/#grit )
+//
+//======================================================================
+
+#ifndef GRIT_MAPPSMOL_H
+#define GRIT_MAPPSMOL_H
+
+#define mappSmolTilesLen 1120
+extern const unsigned short mappSmolTiles[560];
+
+#define mappSmolMapLen 2048
+extern const unsigned short mappSmolMap[1024];
+
+#define mappSmolPalLen 512
+extern const unsigned short mappSmolPal[256];
+
+#endif // GRIT_MAPPSMOL_H
+
+//}}BLOCK(mappSmol)
--- template/source/template.c
+++ template/source/template.c
@@ -1,6 +1,7 @@
 #include <string.h>
 #include "toolbox.h"
 #include "happySquare.h"
+#include "happy.h"
 #include "mapp.h"
 
 #define MAP_WIDTH 32*16
@@ -8,24 +9,27 @@
 OBJ_ATTR obj_buffer[128];
 OBJ_AFFINE *obj_aff_buffer = (OBJ_AFFINE *) obj_buffer;
 
-int const firstHorizontalOffset = 192;
+int const firstHorizontalOffset = 10;
 int const firstVerticalOffset = 64;
 int currentHorizontalOffset = firstHorizontalOffset;
 int currentVerticalOffset = firstVerticalOffset;
-int newMap[16][32];
 
-s32 playerLocation[2];
+s32 playerLocationX;
+s32 playerLocationY;
 s32 isJumping = 0;
 s32 gravity = 1;
 s32 verticalSpeed = 6;
 s32 horizontalSpeed = 1;
-s32 firstVerticalSpeed = 6;
-s32 firstHorizontalSpeed = 1;
-s32 collisionX = 0;
-s32 collisionY = 0;
+const s32 firstVerticalSpeed = 6;
+const s32 firstHorizontalSpeed = 1;
+s32 collisionGround = 0; //ground
+s32 collisionRoof = 0; // roof
+s32 collisionRight = 0;
+s32 collisionLeft = 0;
 int x = 0;
 int timer = 0;
 
+//doesn't work
 void jump() {
     if (!isJumping) {
         isJumping = 1;
@@ -43,10 +47,10 @@ void jump() {
         }
     }
 
-    if (isJumping && collisionX) { //there is collisionX on the bottom part of the player
+    if (!collisionGround && verticalSpeed != firstVerticalSpeed) { // hits the ground
         isJumping = 0; //stops jumping
     } else {
-        playerLocation[1] -= verticalSpeed;
+        playerLocationY -= verticalSpeed;
     }
 }
 
@@ -56,13 +60,22 @@ void run() {
     }
 }
 
-// testing a few sprite things
-// D-pad: move and horizontal flip
-// UP: JUMP
+//only works for the first half of the map due to the array mappMap[] being organized in a special way
+s32 checkCollision(s32 coordX, s32 coordY, s32 spriteOffsetX, s32 spriteOffSetY) {
+
+    return (mappMap[32 * (coordY + spriteOffSetY) + (coordX + spriteOffsetX)] != 0x00000000);
+
+}
+
+/* testing a few sprite things
+ D-pad: move and horizontal flip
+ UP: JUMP
+ B: RUN
+*/
 _Noreturn void obj_test() {
     int isTurnedLeft = 0;
-    playerLocation[0] = 10; //x = 10
-    playerLocation[1] = 100; // y = 127
+    playerLocationX = 10; //x = 10
+    playerLocationY = 100; // y = 127
 
     u32 tid = 0, pb = 0;        // tile id, pal-bank
 
@@ -86,8 +99,8 @@ _Noreturn void obj_test() {
 
         // move left
         // flip horizontally if looking to the right
-        if ((key_held(KEY_LEFT) || key_hit(KEY_LEFT)) && !collisionY) {
-            playerLocation[0] -= horizontalSpeed;
+        if ((key_held(KEY_LEFT) || key_hit(KEY_LEFT)) && !collisionLeft) {
+            playerLocationX -= horizontalSpeed;
             if (!isTurnedLeft) {
                 happy->attr1 ^= ATTR1_HFLIP;
                 isTurnedLeft = 1;
@@ -96,104 +109,116 @@ _Noreturn void obj_test() {
 
         // move right
         // flip horizontally if looking to the left
-        if ((key_held(KEY_RIGHT) || key_hit(KEY_RIGHT)) && !collisionY) {
-            playerLocation[0] += horizontalSpeed;
+        if ((key_held(KEY_RIGHT) || key_hit(KEY_RIGHT)) && !collisionRight) {
+            playerLocationX += horizontalSpeed;
             if (isTurnedLeft) {
                 happy->attr1 ^= ATTR1_HFLIP;
                 isTurnedLeft = 0;
             }
         }
 
-       /* if (collisionX) {
+        //has a roof
+        if (collisionRoof) {
             happy->attr1 ^= ATTR1_VFLIP;
-            playerLocation[1] -= 1;
-            // make it glow (via palette swapping)
         }
 
-        if (collisionY) {
+        //has a wall on the way
+        if (collisionGround) {
             pb = 1;
         } else {
             pb = 0;
         }
-*/
-        if (!collisionX && !isJumping) {
-            playerLocation[1] += 1;
-        }
 
-        //simple jump with gravity (no collisionX)
-        if (key_hit(KEY_UP) || key_held(KEY_UP) || isJumping) {
+        //simple jump with gravity (no collisionGround)
+        if ((key_hit(KEY_UP) || isJumping) && !collisionRoof) {
             jump();
         }
 
-        if (key_hit(KEY_A)) {
-            playerLocation[1] = 110;
-        }
-
+        //running
         if (key_held(KEY_B)) {
             run();
-        }
-
-        if (!key_held(KEY_B)) {
+        } else {
             horizontalSpeed = firstHorizontalSpeed;
         }
 
         //background right scroller
-        if (playerLocation[0] >= 110) {
+        if (playerLocationX >= 110 && !collisionRight) {
             x += horizontalSpeed;
             REG_BG0HOFS = x + firstHorizontalOffset; // dx is the offset
             currentHorizontalOffset = x + firstHorizontalOffset; // saves the value of the ofset
             if (horizontalSpeed == 2) {
-                playerLocation[0] = 110 - horizontalSpeed + 1; // inbetween frame
+                playerLocationX = 110 - horizontalSpeed + 1; // inbetween frame
             } else {
-                playerLocation[0] = 110 - horizontalSpeed;
+                playerLocationX = 110 - horizontalSpeed;
             }
         }
 
         //background left scroller
-        if (playerLocation[0] <= 40) {
+        if (playerLocationX <= 40 && !collisionLeft) {
             x -= horizontalSpeed;
             REG_BG0HOFS = x + firstHorizontalOffset;
             currentHorizontalOffset = x + firstHorizontalOffset;
             if (horizontalSpeed == 2) {
-                playerLocation[0] = 40 + horizontalSpeed - 1; // inbetween frame
+                playerLocationX = 40 + horizontalSpeed - 1; // inbetween frame
             } else {
-                playerLocation[0] = 40 + horizontalSpeed;
+                playerLocationX = 40 + horizontalSpeed;
             }
         }
 
-        //collisionX = collisionX_test(playerLocation[0], playerLocation[1], playerLocation[0]+31, playerLocation[1]+63, horizontalSpeed, verticalSpeed);
+        // get the map coords
+        int coordX = (currentHorizontalOffset + playerLocationX) / 16;
+        int coordY = (currentVerticalOffset + playerLocationY) / 16;
 
-        int coordX = (currentHorizontalOffset + playerLocation[0]) / 16;
-        int coordY = (currentVerticalOffset + playerLocation[1]) / 16;
-
-        if ((newMap[coordY][coordX + 1] != 0x00000000)) {
-            collisionY = 1;
-            playerLocation[0] -= 2;
+        //x+1 (right wall)
+        if (checkCollision(coordX, coordY, 1, 0) || checkCollision(coordX, coordY, 1, 1)) {
+            collisionRight = 1;
+            if (!collisionRoof) {
+                playerLocationX--;
+            }
         } else {
-            collisionY = 0;
-        }
-        if (newMap[coordY][coordX - 1] != 0x00000000) {
-            collisionY = 1;
-            playerLocation[0] += 2;
-        } else {
-            collisionY = 0;
+            collisionRight = 0;
         }
 
-        if (newMap[coordY + 1][coordX] != 0x00000000) {
-            collisionX = 1;
-            playerLocation[1] -= 2;
+        //x-1 (left wall)
+        if (checkCollision(coordX, coordY, 0, 0) || checkCollision(coordX, coordY, 0, 1)) {
+            collisionLeft = 1;
+            if (!collisionRoof) {
+                playerLocationX++;;
+            }
+
         } else {
-            collisionX = 0;
+            collisionLeft = 0;
         }
 
-        if (newMap[coordY - 1][coordX] != 0x00000000) {
-            playerLocation[1] += 2;
+        //y-1 (roof)
+        if (checkCollision(coordX, coordY, 0, 0) || checkCollision(coordX, coordY, 1, 0)) {
+            collisionRoof = 1;
+            if (!collisionLeft && !collisionRight) {
+                playerLocationY++;
+            }
+        } else {
+            collisionRoof = 0;
+        }
+
+        //y+1 (ground)
+        if (checkCollision(coordX, coordY, 0, 1) || checkCollision(coordX, coordY, 1, 1)) {
+            collisionGround = 1;
+            if (!collisionLeft && !collisionRight) {
+                playerLocationY--;
+            }
+        } else {
+            collisionGround = 0;
+        }
+
+        //default gravity
+        if (!collisionGround && !isJumping) {
+            playerLocationY += gravity;
         }
 
         // Hey look, it's one of them build macros!
-        // happy->attr2= ATTR2_BUILD(tid, pb, 0);
+
         happy->attr2 = ATTR2_BUILD(tid, pb, 0);
-        obj_set_pos(happy, playerLocation[0], playerLocation[1]);
+        obj_set_pos(happy, playerLocationX, playerLocationY);
 
         oam_copy(oam_mem, obj_buffer, 1);    // only need to update one
     }
@@ -224,39 +249,46 @@ void loadSprite() {
     REG_DISPCNT = DCNT_OBJ | DCNT_OBJ_1D | DCNT_BG0;
 }
 
-static void accessMap() {
-    int i, j, constant, counter = 0;
-
-    while (counter < 512) {
-        for (i = 0; i < 16; i++) {
-            constant = i * 32;
-            int flag = 1;
-            for (j = 0; j < 32; ++j) {
-
-                if (j >= 16 && flag) {
-                    constant += 496;
-                    flag = 0;
-                }
-
-                if (mappMap[constant] == 0) {
-                    newMap[i][j] = 0;
-                } else {
-                    newMap[i][j] = 1;
-                }
-                counter++;
-                constant++;
+/*
+void fillSquare(int i, int j, int value) {
+        int k, l;
+        for (k = 0; k < 16; ++k) {
+            for (l = 0; l < 16; ++l) {
+                newMap[i+k][j+l] = value;
             }
         }
-    }
-}
+
+}*/
+/*
+void accessMap() {
+        int i, j, constant, counter = 0;
+
+        while (counter < 512) {
+            for (i = 0; i < 16; i++) {
+                constant = i;// * 32;
+                int flag = 1;
+                for (j = 0; j < 32; ++j) {
+                    if (j >= 16 && flag) {
+                        constant += 496;
+                        flag = 0;
+                    }
+
+                    if (mappMap[constant] == 0) {
+                        newMap[i][j] = 0; // fillSquare(i*16,j*16,0);
+                    } else {
+                        newMap[i][j] = 1; // fillSquare(i*16,j*16,1);
+                    }
+                    counter++;
+                    constant++;
+                }
+            }
+        }
+}*/
 
 int main() {
 
     loadBackGround();
     loadSprite();
-    accessMap();
+    //  accessMap();
     obj_test();
-    while (1);
-
-    return 0;
 }
